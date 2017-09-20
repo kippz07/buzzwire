29f473ac7187947318cabf3612955072f6184245
Jenny Johnson <kippz07@users.noreply.github.com>
2017-09-20 14:44:36 +0100

    game playable without drawing

--- js/main.js
+++ js/main.js
@@ -4,14 +4,38 @@ $(function () {
     var $innerSquare = $("#inner-square");
     var $canvasDiv = document.getElementById("myCanvas");
     var myPath;
+    var $main = $("main");
+    var $breakLine = $("#break-line");
+    var $startLine = $("#start-line");
+    var $endLine = $("#end-line");
+    var $text = $("#text"); 
 
     $square.mouseleave(function (event) {
-        console.log("mouse out of bounds");
+        $text.html("You lose!");
     })
 
+    $innerSquare.mouseover(function (event) {
+        $text.html("You lose!");
+    })
 
+    $breakLine.mouseover(function (event) {
+        $text.html("You lose!");
+    })
 
+    $endLine.mouseover(function (event) {
+        $text.html("You win!");
+    })
 
+    $startLine.mouseover(function (event) {
+        $text.html("");
+    })
+
+    var rotate = 0;
+    setInterval(function () {
+      rotate += 2;
+      $square.css('transform', 'rotateZ(' + rotate + 'deg)');
+
+    }, 100);
 
 
 
@@ -40,15 +64,15 @@ $(function () {
     $canvas = $("#canvas")
 
     $canvas.mousedown(function(e) {
-            
+            console.log("clicked")
       paint = true;
-      addClick(e.clientX - 170, e.clientY - 60);
+      addClick(e.clientX - 235, e.clientY - 60);
       redraw();
     });
 
     $canvas.mousemove(function(e) {
       if(paint){
-        addClick(e.pageX - 170, e.pageY - 60, true);
+        addClick(e.pageX - 235, e.pageY - 60, true);
         redraw();
       }
     });
